52bdb3870658e69f98da5ae57884ddf8c5ef4a61
j2938216501 <k29382165@gmail.com>
2026-05-12 15:18:01 +0800

    Add files via upload
    更新Render上版_20260512

--- line_bot/functions/api/linebot_main_v2備份.js
+++ line_bot/functions/api/linebot_main_v2備份.js
@@ -22,24 +22,10 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
 
 
-// 初始化 Firebase 
-// if (!admin.apps.length) {
-//     admin.initializeApp();
-// }
-
-// 初始化 Firebase Admin-Render
+// 初始化 Firebase Admin
 if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        }),
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    });
+    admin.initializeApp();
 }
-
-
 const bucket = admin.storage().bucket();
 
 // LINE Bot 設定
