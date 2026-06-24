57d26e4a8cb85ba7f3e257370b5ebc63d4dde132
j2938216501 <k29382165@gmail.com>
2026-06-24 16:52:43 +0800

    修firebase相關_v12_20260624
    修firebase相關_v12_20260624

--- line_bot/functions/api/linebot_language/word.js
+++ line_bot/functions/api/linebot_language/word.js
@@ -40,6 +40,7 @@ if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON && !process.env.GOOGLE_APPLI
 if (!admin.apps.length) {
     admin.initializeApp({
     credential: credentialWithGlobalFetch({
+        projectId: process.env.FIREBASE_PROJECT_ID, 
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
